c66c9d5974ce4f5ce0c1b5750e16bbf6c5480038
Kristian Carbonaro <carbonarok@gmail.com>
2025-11-04 18:11:01 +0000

    Added sleep function

--- fluxus_core_functions/__init__.py
+++ fluxus_core_functions/__init__.py
@@ -1,6 +1,7 @@
 from fluxus_core_functions.api.get_request import get_request
 from fluxus_core_functions.api.post_request import post_request
 from fluxus_core_functions.core.print import print
+from fluxus_core_functions.core.sleep import sleep
 from fluxus_core_functions.git.clone_inventory import clone_inventory
 from fluxus_core_functions.snapshot.compare_snapshot import compare_snapshots
 from fluxus_core_functions.snapshot.create_snapshot import create_snapshot
@@ -11,6 +12,7 @@ __fluxus__ = [
     get_request,
     post_request,
     print,
+    sleep,
     clone_inventory,
     compare_snapshots,
     create_snapshot,
