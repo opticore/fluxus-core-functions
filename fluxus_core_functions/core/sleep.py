b781b4ce9dfb44251a5dbf0a21060cd6f8301c53
Kristian Carbonaro <carbonarok@gmail.com>
2025-11-04 18:09:18 +0000

    Added sleep function

---
+++ fluxus_core_functions/core/sleep.py
@@ -0,0 +1,18 @@
+import time
+from typing import Annotated
+
+from fluxus_sdk.func import fluxus_func
+from fluxus_sdk.logger import logger
+
+
+@fluxus_func(
+    name="sleep",
+    description="Sleep for a given amount of time",
+    dir_path="fluxus_core_functions/core",
+)
+def sleep(
+    seconds: Annotated[int, "Number of seconds to sleep"],
+):
+    logger.info(f"Sleeping for {seconds} seconds")
+    time.sleep(seconds)
+    logger.info(f"Slept for {seconds} seconds")
